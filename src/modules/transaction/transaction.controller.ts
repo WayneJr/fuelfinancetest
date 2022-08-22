63ef621c4962f1104241396f13ff301495d9d8ad
WayneJr <40968414+WayneJr@users.noreply.github.com>
2022-08-22 06:29:50 +0100

    added uthorization guards and updated readme

--- src/modules/transaction/transaction.controller.ts
+++ src/modules/transaction/transaction.controller.ts
@@ -5,25 +5,29 @@ import {
   HttpStatus,
   Post,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { TransactionService } from './transaction.service';
+import { JwtGuard } from '../auth/guards/jwt.guard';
 
 @Controller('transaction')
 export class TransactionController {
-  constructor(private readonly transactionServiice: TransactionService) {}
+  constructor(private readonly transactionService: TransactionService) {}
 
   @HttpCode(HttpStatus.CREATED)
   @Post('file')
+  // @UseGuards(JwtGuard) // You can uncomment this to test endpoint authorization
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
-    return await this.transactionServiice.parseCsvFile(file);
+    return await this.transactionService.parseCsvFile(file);
   }
 
   @HttpCode(HttpStatus.OK)
   @Get('report')
+  // @UseGuards(JwtGuard) // You can uncomment this to test endpoint authorization
   async getReports() {
-    return await this.transactionServiice.getReports();
+    return await this.transactionService.getReports();
   }
 }
